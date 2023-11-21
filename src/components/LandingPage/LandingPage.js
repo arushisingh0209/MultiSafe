251d635129b0d416cfeea16d6e6f97039095d68b
arushisingh0209 <arushisingh@jklu.edu.in>
2023-11-21 19:47:37 +0530

    First Page design

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import NavBar from './NavBar';
+import logo from "./logo.png";
+
 const LandingPage = () => {
   return (
-    <div>
-      <NavBar />
+    <div className="flex flex-row" style={{ backgroundColor: "#FCFAD1" }}>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(/images/multisafeBG.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "left",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          width: "60%",
+        }}
+      ></div>
+      <div className="flex flex-column flex-wrap align-end items-center justify-center pb-28" style={{ width: "40%" }}>
+        <img src={logo} className='w-full' alt="Logo" />
+        <button className="btn btn-wide" style={{backgroundColor:"#72693E",color:"#FCFAD1"}}>Connect To Wallet</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
